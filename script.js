3557dc63d5097df6e61dc8841deb0ae3c3edcc43
Ярослав <mulo-_-2@mail.ru>
2018-07-13 00:18:22 +0300

    Added action on mauseclick

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-  function playSound (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  function playS (e) {
     const key = document.querySelector(`.soundboard_keyboard-button[data-key="${e.keyCode}"]`);
+    key.classList.add('active');
+    playSound(e.keyCode);
+    console.log("playS"+e.keyCode);
+  }
+  function playSound(keyCode) {
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
     if (!audio)
       return;
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('active');
+    console.log(keyCode);
   }
-
   function endSound (e) {
     const key = document.querySelector(`.soundboard_keyboard-button[data-key="${e.keyCode}"]`);
     if(!key)
       return;
     key.classList.remove('active');
   }
-
-
+  $(".soundboard_keyboard-button").click(function(){
+      var keyCode=$(this).data('key');
+      playSound(keyCode);
+      keyCode=0;
+  });
   window.addEventListener('mousedown;', playSound)
   window.addEventListener('mouseup', endSound)
-  window.addEventListener('keydown', playSound)
+  window.addEventListener('keydown', playS)
   window.addEventListener('keyup', endSound)
